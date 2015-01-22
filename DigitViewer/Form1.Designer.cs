b351a5fb2259009785ac978b391bc75788333ad1
incorux <incorux@gmail.com>
2015-01-23 00:02:01 +0100

    Init

---
+++ DigitViewer/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace DigitViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialogTrain = new System.Windows.Forms.Button();
+            this.Holder = new System.Windows.Forms.TableLayoutPanel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialogTrain
+            // 
+            this.openFileDialogTrain.Location = new System.Drawing.Point(565, 2);
+            this.openFileDialogTrain.Name = "openFileDialogTrain";
+            this.openFileDialogTrain.Size = new System.Drawing.Size(143, 23);
+            this.openFileDialogTrain.TabIndex = 0;
+            this.openFileDialogTrain.Text = "Load";
+            this.openFileDialogTrain.UseVisualStyleBackColor = true;
+            this.openFileDialogTrain.Click += new System.EventHandler(this.openFileDialogTrain_Click);
+            // 
+            // Holder
+            // 
+            this.Holder.ColumnCount = 1;
+            this.Holder.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.Holder.Location = new System.Drawing.Point(-1, 2);
+            this.Holder.Name = "Holder";
+            this.Holder.RowCount = 1;
+            this.Holder.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.Holder.Size = new System.Drawing.Size(560, 560);
+            this.Holder.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(579, 31);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 566);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.Holder);
+            this.Controls.Add(this.openFileDialogTrain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFileDialogTrain;
+        private System.Windows.Forms.TableLayoutPanel Holder;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ DigitViewer/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using MLP_approximation;
+
+namespace DigitViewer
+{
+    public partial class Form1 : Form
+    {
+        private double[,] trainData;
+        private DigitImage[] trainImages;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static DigitImage[] LoadMnistData(string pixelFile, string labelFile)
+        {
+            byte[][] pixels = new byte[28][];
+            for (int i = 0; i < pixels.Length; ++i)
+                pixels[i] = new byte[28];
+            FileStream ifsPixels = new FileStream(pixelFile, FileMode.Open);
+            FileStream ifsLabels = new FileStream(labelFile, FileMode.Open);
+            BinaryReader brImages = new BinaryReader(ifsPixels);
+            BinaryReader brLabels = new BinaryReader(ifsLabels);
+            int magic1 = brImages.ReadInt32(); // stored as big endian
+            int imageCount = brImages.ReadInt32();
+            imageCount = ReverseBytes(imageCount);
+            DigitImage[] result = new DigitImage[imageCount];
+            int numRows = brImages.ReadInt32();
+            numRows = ReverseBytes(numRows);
+            int numCols = brImages.ReadInt32();
+            numCols = ReverseBytes(numCols);
+            int magic2 = brLabels.ReadInt32();
+            magic2 = ReverseBytes(magic2);
+            int numLabels = brLabels.ReadInt32();
+            numLabels = ReverseBytes(numLabels);
+            for (int di = 0; di < imageCount; ++di)
+            {
+                for (int i = 0; i < 28; ++i) // get 28x28 pixel values
+                {
+                    for (int j = 0; j < 28; ++j)
+                    {
+                        byte b = brImages.ReadByte();
+                        pixels[i][j] = b;
+                    }
+                }
+                byte lbl = brLabels.ReadByte(); // get the label
+                DigitImage dImage = new DigitImage(28, 28, pixels, lbl);
+                result[di] = dImage;
+            } // Each image
+            ifsPixels.Close();
+            brImages.Close();
+            ifsLabels.Close();
+            brLabels.Close();
+            return result;
+        }
+
+        public static int ReverseBytes(int v)
+        {
+            byte[] intAsBytes = BitConverter.GetBytes(v);
+            Array.Reverse(intAsBytes);
+            return BitConverter.ToInt32(intAsBytes, 0);
+        }
+
+        private void openFileDialogTrain_Click(object sender, EventArgs e)
+        {
+            var openFileDialogTrain = new OpenFileDialog();
+            if (openFileDialogTrain.ShowDialog() != DialogResult.OK) return;
+            var numberOfInputs = 784;
+            trainData = new double[60, numberOfInputs + 1];
+            string pixelFile = openFileDialogTrain.FileName;
+            string labelFile = Path.GetDirectoryName(pixelFile) + @"\train-labels.idx1-ubyte";
+            trainImages = LoadMnistData(pixelFile, labelFile);
+
+            Display(0);
+        }
+
+        private void Display(int index)
+        {
+            if (trainImages == null || index >= trainImages.Length) return;
+            var image = trainImages[index];
+            var size = 14;
+            var justMade = false;
+            if (Holder.Controls.Count == 0)
+            {
+                Holder.Controls.Clear();
+                Holder.ColumnCount = 28;
+                Holder.RowCount = 28;
+                justMade = true;
+            }
+            for (int j = 0; j < image.pixels.Length; j++)
+            {
+                var pixel = image.pixels[j];
+                for (int i = 0; i < pixel.Length; i++)
+                {
+                    var bytes = pixel[i];
+
+                    var pb = justMade
+                        ? new PictureBox
+                          {
+                              Width = size,
+                              Height = size
+                          }
+                        : Holder.Controls[i * 28 + j];
+
+                    pb.BackColor = bytes > 0 ? Color.Black : Color.White;
+                    if (justMade) Holder.Controls.Add(pb, j, i);
+                    else pb.Update();
+                }
+            }
+            Holder.Update();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            var control = (NumericUpDown)sender;
+            Display((int)control.Value);
+        }
+    }
+
+}
